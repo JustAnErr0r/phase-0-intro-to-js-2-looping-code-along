2eee2fb43ccd2352ff5e23f53a86a7dbd993daed
Madeleine Heineman <mheineman16@gmail.com>
2022-09-04 21:41:06 -0400

    commit tree 75a344ef9784f528e4a48cd8d8266ecf316b9000

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+
+function writeCards(name, event) {
+    const messages = []
+    for (let i = 0; i < name.length; i++){
+        const message = (`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+        messages.push(message)
+    }
+    return messages
+}
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], 'surprise'))
+
+// function countDown() {
+//     for (let i = 10; i >= 0, i--) 
+// }
+
+
+function countdown(){
+    let num = "";   
+    while( i >= 0)
+    {   
+        num += i;
+        if(i > 0) {
+            num += " "; 
+        }
+        i--;
+    }
+    return num;
+}
+const result = countdown(4);  
+
+console.log(result);
